cab9cc809c577e527a49f0d3a31da7837448d612
cjyotirmoy <jyotirmoyc@inryo>
2021-04-14 22:04:56 +0530

    changed wallpaper type:Fill

--- WinScript/unsplashWallpaper.ps1
+++ WinScript/unsplashWallpaper.ps1
@@ -6,7 +6,7 @@ Write-Output "Gimme a minute..."
 Remove-Item -Force C:\Users\$user\OneDrive\Pictures\unsplash.jpg
 $client = new-object System.Net.WebClient
 try{
-   $client.DownloadFile("https://source.unsplash.com/1920x1080/?lanscape,mountain,night","$Value")
+   $client.DownloadFile("https://source.unsplash.com/1440x900/?mountains","$Value")
 }
 catch [System.Net.WebException] {
    if ($_.Exception.InnerException) {
@@ -18,46 +18,76 @@ catch [System.Net.WebException] {
 #Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $Value
 #Start-Sleep -Seconds 5
 #rundll32.exe user32.dll, UpdatePerUserSystemParameters 1 ,True
-Add-Type @"
-using System;
-using System.Runtime.InteropServices;
-using Microsoft.Win32;
-namespace Wallpaper
-{
-   public enum Style : int
-   {
-       Tile, Center, Stretch, NoChange
-   }
-   public class Setter {
-      public const int SetDesktopWallpaper = 20;
-      public const int UpdateIniFile = 0x01;
-      public const int SendWinIniChange = 0x02;
-      [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-      private static extern int SystemParametersInfo (int uAction, int uParam, string lpvParam, int fuWinIni);
-      public static void SetWallpaper ( string path, Wallpaper.Style style ) {
-         SystemParametersInfo( SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange );
-         RegistryKey key = Registry.CurrentUser.OpenSubKey("Control Panel\\Desktop", true);
-         switch( style )
-         {
-            case Style.Stretch :
-               key.SetValue(@"WallpaperStyle", "2") ; 
-               key.SetValue(@"TileWallpaper", "0") ;
-               break;
-            case Style.Center :
-               key.SetValue(@"WallpaperStyle", "1") ; 
-               key.SetValue(@"TileWallpaper", "0") ; 
-               break;
-            case Style.Tile :
-               key.SetValue(@"WallpaperStyle", "1") ; 
-               key.SetValue(@"TileWallpaper", "1") ;
-               break;
-            case Style.NoChange :
-               break;
-         }
-         key.Close();
-      }
-   }
+Function Set-WallPaper {
+ 
+<#
+ 
+    .SYNOPSIS
+    Applies a specified wallpaper to the current user's desktop
+    
+    .PARAMETER Image
+    Provide the exact path to the image
+ 
+    .PARAMETER Style
+    Provide wallpaper style (Example: Fill, Fit, Stretch, Tile, Center, or Span)
+  
+    .EXAMPLE
+    Set-WallPaper -Image "C:\Wallpaper\Default.jpg"
+    Set-WallPaper -Image "C:\Wallpaper\Background.jpg" -Style Fit
+  
+#>
+ 
+param (
+    [parameter(Mandatory=$True)]
+    # Provide path to image
+    [string]$Image,
+    # Provide wallpaper style that you would like applied
+    [parameter(Mandatory=$False)]
+    [ValidateSet('Fill', 'Fit', 'Stretch', 'Tile', 'Center', 'Span')]
+    [string]$Style
+)
+ 
+$WallpaperStyle = Switch ($Style) {
+  
+    "Fill" {"10"}
+    "Fit" {"6"}
+    "Stretch" {"2"}
+    "Tile" {"0"}
+    "Center" {"0"}
+    "Span" {"22"}
+  
 }
-"@
-
-[Wallpaper.Setter]::SetWallpaper( $Value, 0 )
+ 
+If($Style -eq "Tile") {
+ 
+    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
+    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 1 -Force
+ 
+}
+Else {
+ 
+    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
+    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 0 -Force
+ 
+}
+Add-Type -TypeDefinition @" 
+using System; 
+using System.Runtime.InteropServices;
+public class Params
+{ 
+    [DllImport("User32.dll",CharSet=CharSet.Unicode)] 
+    public static extern int SystemParametersInfo (Int32 uAction, 
+                                                   Int32 uParam, 
+                                                   String lpvParam, 
+                                                   Int32 fuWinIni);
+}
+"@ 
+    $SPI_SETDESKWALLPAPER = 0x0014
+    $UpdateIniFile = 0x01
+    $SendChangeEvent = 0x02
+  
+    $fWinIni = $UpdateIniFile -bor $SendChangeEvent
+  
+    $ret = [Params]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $Image, $fWinIni)
+}
+Set-Wallpaper -Image $Value -Style Fill
